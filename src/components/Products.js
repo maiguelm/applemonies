c34ba9a0f9525c523dff1414c397cfd3d72a3736
maiguelm <migmoyano86@gmail.com>
2022-11-10 15:21:02 -0300

    mmm

---
+++ src/components/Products.js
@@ -0,0 +1,44 @@
+import productsArray from "./productsArray.json"
+
+let products = []
+
+products = productsArray.map((item) => {
+	return item
+})
+
+
+export const getProducts = () => {
+	let arrayPetition = new Promise((res) => {
+		setTimeout(() => {
+			res(products)
+		}, 500)
+
+	})
+	return arrayPetition
+}
+
+export const getProductByCategory = (categoria) => {
+
+	let arrayPetition = new Promise((res) => {
+		setTimeout(() => {
+			res(products.filter((item) => item.categoria == categoria)) 
+		}, 1500)
+	})
+	return arrayPetition
+}
+
+export const getProductsById = (id) => {
+    let arrayPetition = new Promise((res) => {
+        setTimeout(() => {
+            res(products.find(item => item.id == id))
+        }, 500)
+		console.log(products)
+    })
+    return arrayPetition
+}
+
+export default {
+    getProducts,
+    getProductByCategory,
+	getProductsById
+}
